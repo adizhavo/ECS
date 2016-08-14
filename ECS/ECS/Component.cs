d36b35be596aa45dac4ed5df80404f82af163de8
Adi Zhavo <adi@gram.gs>
2016-08-14 13:56:37 +0300

    Added Entity and component interface.

---
+++ ECS/ECS/Component.cs
@@ -0,0 +1,4 @@
+﻿public interface IComponent
+{
+    Entity entity {set;get;}
+}
---
+++ ECS/ECS/Entity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Entity
+{
+    public List<IComponent> components
+    {
+        private set;
+        get;
+    }
+
+    public Entity()
+    {
+        components = new List<IComponent>();
+    }
+
+    public void AddComponent(IComponent newCompoent)
+    {
+        if (newCompoent == null)
+        {
+            Console.Write("Component that you intented to add is null, method will return void");
+            return;
+        }
+
+        components.Add(newCompoent);
+        newCompoent.entity = this;
+    }
+
+    public void RemoveComponent<T>() where T : class, IComponent
+    {
+        for (int i= 0; i < components.Count; i ++)
+        {
+            IComponent cmp = components[i];
+
+            if (cmp is T) 
+            {
+                cmp.entity = null;
+                components.RemoveAt(i);
+            }
+        }
+    }
+
+    public void RemoveAllComponent()
+    {
+        for (int i= 0; i < components.Count; i ++)
+        {
+            components[i].entity = null;
+            components.RemoveAt(i);
+        }
+    }
+
+    public bool HasComponent<T>() where T : class, IComponent
+    {
+        foreach(IComponent cmp in components)
+        {
+            if (cmp is T) 
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
